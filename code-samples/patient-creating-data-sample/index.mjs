b5043a9b7e0efee50aebbb464db523234abe6b8b
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-09-29 13:52:10 +0200

    Added code sample

---
+++ code-samples/patient-creating-data-sample/index.mjs
@@ -0,0 +1,54 @@
+import "isomorphic-fetch";
+import { CodingReference, HealthcareElement, medTechApi } from '@icure/medical-device-sdk';
+import { webcrypto } from "crypto";
+import { hex2ua } from "@icure/api";
+import { tmpdir } from "os";
+import { TextDecoder, TextEncoder } from "util";
+import { LocalStorage } from 'node-localstorage';
+;
+global.localStorage = new LocalStorage(tmpdir(), 5 * 1024 ** 3);
+global.Storage = '';
+global.TextDecoder = TextDecoder;
+global.TextEncoder = TextEncoder;
+const password = process.env.PASSWORD;
+const userName = process.env.USERNAME;
+const privKey = process.env.PRIV_KEY;
+const hcpId = process.env.HCP_ID;
+const patientId = process.env.PATIENT_ID;
+const host = 'https://kraken.icure.dev/rest/v1';
+const api = await medTechApi()
+    .withICureBasePath(host)
+    .withUserName(userName)
+    .withPassword(password)
+    .withCrypto(webcrypto)
+    .build();
+await api.cryptoApi.loadKeyPairsAsTextInBrowserLocalStorage(hcpId, hex2ua(privKey));
+const patient = await api.patientApi.getPatient(patientId);
+const healthcareElement = await api.healthcareElementApi.createOrModifyHealthcareElement(new HealthcareElement({
+    description: 'My diagnosis is that the patient has Hay Fever',
+    codes: new Set([
+        new CodingReference({
+            id: 'SNOMEDCT|21719001|20020131',
+            type: 'SNOMEDCT',
+            code: '21719001',
+            version: '20020131'
+        })
+    ])
+}), patient.id);
+// await api.dataSampleApi.createOrModifyDataSampleFor(
+// 	patient.id,
+// 	new DataSample({
+// 		content: { 'en': new Content({
+// 				stringValue: 'The patient has fatigue'
+// 			})},
+// 		codes: new Set([
+// 			new CodingReference({
+// 				id: 'SNOMEDCT|84229001|20020131',
+// 				type: 'SNOMEDCT',
+// 				code: '84229001',
+// 				version: '20020131'
+// 			})
+// 		]),
+// 		healthcareElementIds: new Set([healthcareElement.id])
+// 	})
+// )
