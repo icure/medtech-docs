f1348e8db355a899312fbdcd8fbf528de3e789dc
Larysa Shashuk <larisa.shashuk@gmail.com>
2023-04-05 13:24:45 +0200

    optimized code for the footer

--- src/theme/Footer/Layout/index.js
+++ src/theme/Footer/Layout/index.js
@@ -3,7 +3,25 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import styles from './index.module.css'
 
-export default function FooterLayout({ style, links, logo, copyright }) {
+export default function FooterLayout({ style }) {
+
+  const footerLinks = [
+    {
+      title: 'Want to know more about iCure?',
+      link: {
+        title: 'Visit our website',
+        url: 'https://icure.com/'
+      }
+    },
+    {
+      title: 'Having a problem?',
+      link: {
+        title: 'Contact us',
+        url: 'https://icure.com/'
+      }
+    }
+  ]
+
   return (
     <footer
       className={clsx('footer', {
@@ -12,24 +30,16 @@ export default function FooterLayout({ style, links, logo, copyright }) {
       <div className="container container-fluid">
         <div className={styles.links}>
 
-          <div className={styles.links__item}>
-            <p>Want to know more about iCure?</p>
-            <svg width="31" height="6" viewBox="0 0 31 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M1 3H31Z" fill="#D7D7D7" />
-              <path d="M1 2.5H0.5V3.5H1V2.5ZM31 3L26 0.113249V5.88675L31 3ZM1 3.5H26.5V2.5H1V3.5Z" fill="#D7D7D7" />
-            </svg>
-            <Link to='https://icure.com/'>Visit our website</Link>
-          </div>
-
-          <div className={styles.links__item}>
-            <p>Having a problem?</p>
-            <svg width="31" height="6" viewBox="0 0 31 6" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M1 3H31Z" fill="#D7D7D7" />
-              <path d="M1 2.5H0.5V3.5H1V2.5ZM31 3L26 0.113249V5.88675L31 3ZM1 3.5H26.5V2.5H1V3.5Z" fill="#D7D7D7" />
-            </svg>
-            <Link to='https://icure.com/'>Contact us</Link>
-          </div>
-
+          {
+            footerLinks.map(el => <div className={styles.links__item}>
+              <p>{el.title}</p>
+              <svg width="31" height="6" viewBox="0 0 31 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M1 3H31Z" fill="#D7D7D7" />
+                <path d="M1 2.5H0.5V3.5H1V2.5ZM31 3L26 0.113249V5.88675L31 3ZM1 3.5H26.5V2.5H1V3.5Z" fill="#D7D7D7" />
+              </svg>
+              <Link to={el.link.url}>{el.link.title}</Link>
+            </div>)
+          }
 
         </div>
       </div>
